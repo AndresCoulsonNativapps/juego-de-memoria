97d9692b03deab18df5cf3704c03c706c55f1720
Andredilla <acoulson@iMac-de-LabSoftware04.local>
2016-06-30 15:13:26 -0500

    subiendo playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for  a in 0...100 {
+    
+    if( a % 5 == 0){
+    print(a," Bingo!!!")
+    }
+    if(a % 2 == 0){
+        print(a," par!!!")
+    }
+    if(a % 2 != 0){
+        print(a," impar!!!")
+    }
+    
+    if(a >= 30 && a <= 40 ){
+        print(a,"Viva Swift!!!")
+    }
+    
+}
